92e423c99eda16ded564be93f7db6577dda715e4
Afthab Anthas <afthab.anthasm@outlook.com>
2025-03-03 20:29:59 +0400

    FIXED broken_travel.py

--- broken_travel.py
+++ broken_travel.py
@@ -6,11 +6,11 @@
 # if he is from the distant past (before 1900),
 # the present era (1900-2020) or from the far future (beyond 2020).
 
-year = int(input("Greetings! What is your year of origin? "))
+year = int(input("Greetings! What is your year of origin? ")) ##Afthab M01033122
 
-if year <= 1900:
-    print("Woah, that's the past!")
-elif year > 1900 and year < 2020:
+if year <= 1900: ##Afthab M01033122
+    print("Woah, that's the past!") ##Afthab M01033122
+elif year > 1900 and year < 2020: ##Afthab M01033122
     print ("That's totally the present!")
-elif year >= 2020:
+elif year >= 2020: ##Afthab M01033122
     print ("Far out, that's the future!!")
